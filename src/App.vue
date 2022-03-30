b0db007cdfb04cbb61f5a5b704ade641a633ee66
soulFool <1832425172@qq.com>
2022-03-31 01:29:38 +0800

    chore(assets,components,main): 项目基础代码编写
    编写scss文件，引入图片，创建header组件，引入全局样式文件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
-  <div class="app">app</div>
+  <m-header></m-header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Header from '@/components/content/header/header.vue'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  components: {
+    MHeader: Header
+  }
 })
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
 
+// 引入全局样式文件
+import '@/assets/scss/index.scss'
+
 createApp(App).use(createPinia()).use(router).mount('#app')
