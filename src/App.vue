83df879b401be47ba3af4b122ad424608e917d71
soulFool <1832425172@qq.com>
2022-03-31 02:13:27 +0800

    chore(router): 完成路由配置，引用组件
    配置路由映射关系，写路由占位符，完成基础视图

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <m-header></m-header>
+  <tab></tab>
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Header from '@/components/content/header/header.vue'
+import Tab from '@/components/content/tab/tab.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    MHeader: Header
+    MHeader: Header,
+    Tab
   }
 })
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,33 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
-const routes: RouteRecordRaw[] = []
+import Recommend from '@/views/recommend.vue'
+import Singer from '@/views/singer.vue'
+import TopList from '@/views/top-list.vue'
+import Search from '@/views/search.vue'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    redirect: '/recommend'
+  },
+  {
+    path: '/recommend',
+    component: Recommend
+  },
+  {
+    path: '/singer',
+    component: Singer
+  },
+  {
+    path: '/top-list',
+    component: TopList
+  },
+  {
+    path: '/search',
+    component: Search
+  }
+]
 
 const router = createRouter({
   routes,
