aaaa995f21d23ea9983d61c5cb9df80728cb797d
soulFool <1832425172@qq.com>
2022-04-01 00:12:56 +0800

    chore(recommend): 轮播图组件的使用

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="recommend">推荐页面</div>
+  <div class="recommend">
+    <div class="slider-wrapper">
+      <div class="slider-content">
+        <!-- 由于数据传入是异步的，而且 better-scroll/slide 在初始化的时候要求至少有一条以上的数据，所以用 v-if 使元素先不渲染 -->
+        <slider v-if="sliders.length" :sliders="sliders"></slider>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,15 +14,44 @@ import { defineComponent } from 'vue'
 
 import { getRecommend } from '@/service/recommend'
 
+import Slider from '@/components/common/slider/slider.vue'
+
 export default defineComponent({
   name: 'recommend',
-  async setup() {
+  components: {
+    Slider
+  },
+  data() {
+    return {
+      sliders: []
+    }
+  },
+  async created() {
     const result = await getRecommend()
-    console.log(result)
-
-    return {}
+    this.sliders = result.sliders
   }
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.recommend {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  .slider-wrapper {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 40%;
+    overflow: hidden;
+    .slider-content {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
