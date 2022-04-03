7e0415ae35c2b303b32329d01a856f6bad85c654
soulFool <1832425172@qq.com>
2022-04-04 01:12:12 +0800

    feat(singer): 歌手列表快速导航
    完成歌手列表右侧快速导航栏的touchmove事件

--- src/components/common/index-list/use-shortcut.ts
+++ src/components/common/index-list/use-shortcut.ts
@@ -7,6 +7,7 @@ import type { BScroll as BScrollType } from '@better-scroll/core/dist/types/BScr
 import type { IIndexListProps } from './type'
 
 export default function useShortcut(props: IIndexListProps, groupRef: Ref<HTMLUListElement>) {
+  const ANCHOR_HEIGHT = 18
   const scrollRef = ref<InstanceType<typeof scroll>>()
 
   const shortcutList = computed(() => {
@@ -15,19 +16,37 @@ export default function useShortcut(props: IIndexListProps, groupRef: Ref<HTMLUL
     })
   })
 
-  function onShortcutTouchStart(e: Event) {
+  const touch = {
+    y1: 0,
+    y2: 0,
+    anchorIndex: 0
+  }
+
+  function onShortcutTouchStart(e: TouchEvent) {
     const anchorIndex = parseInt((e.target as HTMLElement).dataset.index!)
-    const targetEl = groupRef.value.children[anchorIndex]
+    touch.y1 = e.touches[0].pageY
+    touch.anchorIndex = anchorIndex
+
+    scrollTo(anchorIndex)
+  }
+
+  function onShortcutTouchMove(e: TouchEvent) {
+    touch.y2 = e.touches[0].pageY
+    const delta = (touch.y2 - touch.y1 / ANCHOR_HEIGHT) | 0
+    const anchorIndex = touch.anchorIndex + delta
+
+    scrollTo(anchorIndex)
+  }
+
+  function onShortcutTouchEnd(e: TouchEvent) {
+    const anchorIndex = (e.target as HTMLElement).dataset.index
+  }
+
+  function scrollTo(index: number) {
+    index = Math.max(0, Math.min(shortcutList.value.length - 1, index))
+    const targetEl = groupRef.value.children[index]
     const scroll = scrollRef.value!.scroll as BScrollType
     scroll.scrollToElement(targetEl as HTMLElement, 0, false, false)
-  }
-
-  function onShortcutTouchMove(e: Event) {
-    const anchorIndex = (e.target as HTMLElement).dataset.index
-  }
-
-  function onShortcutTouchEnd(e: Event) {
-    const anchorIndex = (e.target as HTMLElement).dataset.index
   }
 
   return {
