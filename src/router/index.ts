51b56b888d04380259fc471c40e561adefff1a9f
soulFool <1832425172@qq.com>
2022-05-04 14:27:44 +0800

    chore(性能优化): 路由组件异步加载

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
 
-import Recommend from '@/views/recommend.vue'
-import Singer from '@/views/singer.vue'
-import TopList from '@/views/top-list.vue'
-import Search from '@/views/search.vue'
-import SingerDetail from '@/views/singer-detail.vue'
-import Album from '@/views/album.vue'
-import TopDetail from '@/views/top-detail.vue'
-import UserCenter from '@/views/user-center.vue'
+const Recommend = () => import(/* webpackChunkName: "recommend" */ '@/views/recommend.vue')
+const Singer = () => import(/* webpackChunkName: "singer" */ '@/views/singer.vue')
+const TopList = () => import(/* webpackChunkName: "top-list" */ '@/views/top-list.vue')
+const Search = () => import(/* webpackChunkName: "search" */ '@/views/search.vue')
+const SingerDetail = () => import(/* webpackChunkName: "singer-detail" */ '@/views/singer-detail.vue')
+const Album = () => import(/* webpackChunkName: "album" */ '@/views/album.vue')
+const TopDetail = () => import(/* webpackChunkName: "top-detail" */ '@/views/top-detail.vue')
+const UserCenter = () => import(/* webpackChunkName: "user-center" */ '@/views/user-center.vue')
 
 const routes: RouteRecordRaw[] = [
   {
